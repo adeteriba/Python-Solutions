f8dc0003c201cc59b3a264ee09fb3915afcc0853
Ade Teriba <adekunleteriba@hotmail.com>
2018-03-03 19:10:59 +0000

    Add files via upload

---
+++ OddEven.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  3 16:56:29 2018
+
+@author: Ade Teriba
+"""
+#Check to see is the number entered by the user is odd or even
+x = input("Enter a number: ")
+if int(x) % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
