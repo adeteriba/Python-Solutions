83d7881ef182887e6f294bb51b2a5574b3914747
Ade Teriba <adekunleteriba@hotmail.com>
2018-03-04 23:21:58 +0000

    Update MinNumber.py

--- MinNumber.py
+++ MinNumber.py
@@ -5,9 +5,12 @@ Created on Sun Mar  4 22:52:00 2018
 @author: Ade Teriba
 """
 
-x = input('Enter a number: ')
-y = input('Enter a number: ')
-z = input('Enter a number: ')
+#x = input('Enter a number: ')
+#y = input('Enter a number: ')
+#z = input('Enter a number: ')
+for i in range(1,4):
+    i = input('Enter a number: ')
+
 def minfunction():
         print(min(x,y,z),'is the smallest number')
         return ' '
